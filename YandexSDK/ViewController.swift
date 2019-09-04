3f547fb474d5cb2432a3b62c79603ebc1a62dcbb
Ruslan <ruslanfilistovich@gmail.com>
2019-09-04 18:42:10 +0300

    first commit

---
+++ YandexSDK/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YandexSDK
+//
+//  Created by Ruslan on 8/30/19.
+//  Copyright © 2019 Ruslan Filistovich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
